855ac1ea0feb52e9923a257fc3968f84881c5d22
emadam <ldy9037@naver.com>
2022-12-06 08:55:43 +0900

    feat(aws_monitoring): 기본 모니터링 설정 추가
    - CloudTrail 용 S3 버킷 생성 및 CloudTrail 설정
    - 챗봇 생성 후 Slack에 추가
    - sns로 이메일 알림 설정 추가
    
    https://emadam.notion.site/5aa088e57a644aeea3882c5e33912610

---
+++ infra/aws_monitoring/sns.tf
@@ -0,0 +1,82 @@
+locals {
+  sns_topic_arn = var.cloudtrail_enabled && var.sns_topic_enabled ? aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn : ""
+}
+
+data "aws_iam_policy_document" "alarms_sns_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:GetTopicAttributes",
+      "SNS:SetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:RemovePermission",
+      "SNS:DeleteTopic",
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:Publish",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.aws_account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${local.sns_topic_arn}",
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
+resource "aws_sns_topic" "cloudwatch_alarms_for_cloudtrail" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled ? 1 : 0
+
+  name = var.cloudwatch_sns_topic_name
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled ? length(var.sns_topic_subscription_email) : 0
+
+  topic_arn = aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn
+  protocol  = "email"
+  endpoint  = var.sns_topic_subscription_email[count.index]
+}
+
+resource "aws_sns_topic_subscription" "slack" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled && var.chatbot_enabled ? 1 : 0
+
+  topic_arn = aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn
+  protocol  = "https"
+  endpoint  = "https://global.sns-api.chatbot.amazonaws.com"
+}
+
+resource "aws_sns_topic_policy" "alarms" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled ? 1 : 0
+
+  arn    = aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn
+  policy = data.aws_iam_policy_document.alarms_sns_policy.json
+}
+
+module "cis_alarms" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled && var.cis_alarms_enabled ? 1 : 0
+
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/cis-alarms"
+  version = "~> 3.0"
+
+  log_group_name    = var.cloudtrail_cloudwatch_logs_group_name
+  alarm_actions     = [aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn]
+  disabled_controls = var.disabled_controls
+}
