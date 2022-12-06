dcdeedbe565ab82f1dc6d96d02c91d5eb0219feb
emadam <ldy9037@naver.com>
2022-12-07 08:59:26 +0900

    feat(ec2_jenkins): Jenkins 용 EC2 Instance 생성
    - t2.micro/ubuntu20.04로 EC2 Instance 구성
    - network output 값을 받아와 public subnet 지정
    
    https://emadam.notion.site/CICD-Terraform-Cloud-workspace-70ac0474da0f497c89c2bbc95eac0cfc

---
+++ infra/aws_cicd/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.11.0"
+    }
+  }
+
+  cloud {
+    organization = "k8s-app"
+    workspaces {
+      name = "cicd"
+    }
+  }
+
+  required_version = ">= 1.1.7"
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Environment = var.tags_environment
+      IAC         = var.tags_iac
+      Team        = var.tags_team
+      Owner       = var.tags_owner
+    }
+  }
+}
+
+data "tfe_outputs" "network_output" {
+  organization = var.organization_name
+  workspace    = var.network_workspace_name
+}
+
+module "ec2_jenkins" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  count = (var.environment_use) ? 1 : 0
+
+  name = "${var.ec2_jenkins_name}-${count.index + 1}"
+
+  ami                    = var.ec2_jenkins_ami
+  instance_type          = var.ec2_jenkins_instance_type
+  subnet_id              = data.tfe_outputs.network_output.values.public_subnets[count.index]
+  vpc_security_group_ids = []
+}
---
+++ infra/aws_cicd/terraform.tfvars
@@ -0,0 +1,5 @@
+environment_use = true
+
+ec2_jenkins_name          = "jenkins"
+ec2_jenkins_instance_type = "t2.micro"
+ec2_jenkins_ami           = "ami-06eea3cd85e2db8ce"
