451019e0844453da4dcb229b299ff39b7a3c5a01
emadam <ldy9037@naver.com>
2022-12-07 19:59:45 +0900

    fix(route53_zones_id): host zone name 대신 host zone id를 output에 등록

--- infra/aws_dns/outputs.tf
+++ infra/aws_dns/outputs.tf
@@ -1,4 +1,4 @@
-output "route53_zones_name" {
+output "route53_zones_id" {
   description = "route53 host zone"
-  value       = keys(module.route53.route53_zone_zone_id)[0]
+  value       = module.route53.route53_zone_zone_id
 }
