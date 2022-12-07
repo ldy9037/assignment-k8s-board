1355dbfb551b967cdd9674d15c18eb3f0e56c50a
emadam <ldy9037@naver.com>
2022-12-07 19:02:43 +0900

    fix(route53_zones_name): output value에 module 명을 zones에서 route53으로 변경

--- infra/aws_dns/outputs.tf
+++ infra/aws_dns/outputs.tf
@@ -1,4 +1,4 @@
 output "route53_zones_name" {
   description = "route53 host zone"
-  value       = keys(module.zones.route53_zone_zone_id)[0]
+  value       = keys(module.route53.route53_zone_zone_id)[0]
 }
