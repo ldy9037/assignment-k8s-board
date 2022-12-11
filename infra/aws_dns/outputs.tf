dfbdb9cef9ea78c970598b15cd32c433f66e812a
emadam <ldy9037@naver.com>
2022-12-11 15:36:54 +0900

    feat(route53_zones_name): output에 route53 zone name 추가
    - emadam.shop에 해당하는 zone 이름을 output에 추가
    
    https://emadam.notion.site/DNS-f633dbffd9344fe7b47e1754d8645c13

--- infra/aws_dns/outputs.tf
+++ infra/aws_dns/outputs.tf
@@ -1,4 +1,9 @@
 output "route53_zones_id" {
-  description = "route53 host zone"
+  description = "route53 host zone id"
   value       = module.route53.route53_zone_zone_id
+}
+
+output "route53_zones_name" {
+  description = "route53 host zone name"
+  value       = module.route53.route53_zone_name
 }
