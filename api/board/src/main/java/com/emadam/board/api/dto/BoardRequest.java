2ee2a34c15b6248794bdbc9f4385a4680dc96c97
emadam <ldy9037@naver.com>
2022-12-11 15:05:40 +0900

    feat(src): 게시판에 대한 get/create API 생성
    - 아이디로 가져오기, 전체 목록 가져오기, 게시글 생성 API 구현
    - API에 대한 테스트 케이스 작성
    
    https://emadam.notion.site/Backend-6d6201b82796486ca3b6759a99463272

---
+++ api/board/src/main/java/com/emadam/board/api/dto/BoardRequest.java
@@ -0,0 +1,40 @@
+package com.emadam.board.api.dto;
+
+public class BoardRequest {
+    private long id;
+    private String subject;
+    private String content;
+    private String username;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
