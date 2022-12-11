82fa5ba6b9bbbee35a7b857fcbdb5a10973e20ef
emadam <ldy9037@naver.com>
2022-12-11 16:53:28 +0900

    feat(board_cloudfront_distribution): board s3를 origin으로 하는 cloudfront 추가

--- infra/aws_storage/main.tf
+++ infra/aws_storage/main.tf
@@ -68,14 +68,15 @@ module "s3_static_contents" {
   }
 }
 
+/*
 resource "aws_route53_record" "static_content_record" {
   zone_id = values(data.tfe_outputs.dns_output.values.route53_zones_id)[0]
   name    = var.route53_record_name
   type    = var.route53_record_type
   ttl     = var.route53_record_ttl
-  records = [module.s3_static_contents.s3_bucket_website_endpoint]
-}
-
+  records = [aws_cloudfront_distribution.board_cloudfront_distribution.domain_name]
+}*/
+/*
 module "static_contents_acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "4.3.1"
@@ -85,4 +86,66 @@ module "static_contents_acm" {
 
   create_route53_records = var.static_contents_acm_create
   wait_for_validation    = var.static_contents_acm_wfv
+
+  depends_on = [
+    aws_route53_record.static_content_record
+  ]
+}
+*/
+
+resource "aws_cloudfront_origin_access_control" "board_cloudfront_oac" {
+  name                              = "board-cloudfront-origin-access-control"
+  description                       = ""
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "board_cloudfront_distribution" {
+
+  origin {
+    domain_name              = module.s3_static_contents.s3_bucket_website_endpoint
+    origin_access_control_id = aws_cloudfront_origin_access_control.board_cloudfront_oac.id
+    origin_id                = "s3-board"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  aliases = ["${var.route53_record_name}.${values(data.tfe_outputs.dns_output.values.route53_zones_name)[0]}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-board"
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "s3-board"
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "blacklist"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
