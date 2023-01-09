f778d6f9260d108c0b22bea61f13c0414efd7ba7
emadam <ldy9037@naver.com>
2023-01-09 13:16:00 +0900

    feat: output에 github deploy role arn 추가

--- infra/common/iam/outputs.tf
+++ infra/common/iam/outputs.tf
@@ -2,3 +2,8 @@ output "iam_github_oidc_role_name" {
   description = "Github OIDC를 통해 사용될 역할 명"
   value       = module.iam_github_oidc_role.name
 }
+
+output "iam_github_oidc_role_arn" {
+  description = "Github OIDC를 통해 사용될 역할 ARN"
+  value       = module.iam_github_oidc_role.arn
+}
